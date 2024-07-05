06e4d11e5e14a2a93f243b76b129b5334a780afd
Vivek datta <129151221+Viveklagishetty@users.noreply.github.com>
2024-07-05 10:19:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import TaskInput from './components/TaskInput';
 import TaskList from './components/TaskList';
 import './App.css';
